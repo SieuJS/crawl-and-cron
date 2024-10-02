2626b232b54da4d9eab0a71c4f0735b7fe2b7acc
21120321 <gamefun206@gmail.com>
2024-10-02 19:35:06 +0700

    Remove all useless modules and files

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common';
-import { PassengerModule } from './passenger/passenger.module';
+
 import { ConferenceModule } from './conference/conference.module';
 @Module({
     imports: [
         CommonModule,
-        PassengerModule,
         ConferenceModule
     ]
 })
